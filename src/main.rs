cf06da8629e36b6a2b4a95c623c2e761d929ca5a
kaubu <29447633+kaubu@users.noreply.github.com>
2023-09-19 10:09:47 +0000

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use tcod::colors::*;
 use tcod::console::*;
 
@@ -82,6 +83,26 @@ struct Game {
     map: Map,
 }
 
+/// A rectangle on the map, used to characterize a room.
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+}
+
 fn main() {
     let root = Root::initializer()
         .font("arial10x10.png", FontLayout::Tcod)
@@ -104,8 +125,8 @@ fn main() {
 
     // Create the object representing the player
     let player = Object::new(
-        centre_x,
-        centre_y,
+        25,
+        23,
         '@',
         WHITE
     );
@@ -178,14 +199,18 @@ fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Object) -> bool {
 }
 
 fn make_map() -> Map {
-    // Fill map with "unblocked" tiles
+    // Fill map with "blocked" tiles
     let mut map = vec![
-        vec![Tile::empty(); MAP_HEIGHT as usize];
+        vec![Tile::wall(); MAP_HEIGHT as usize];
         MAP_WIDTH as usize
     ];
 
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+    // Create two rooms
+    let room1 = Rect::new(20, 15, 10, 15);
+    let room2 = Rect::new(50, 15, 10, 15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
+    create_h_tunnel(25, 55, 23, &mut map);
 
     map
 }
@@ -228,4 +253,27 @@ fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
         1.0,
         1.0,
     );
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    // Go through the tiles in the rectangle and make them passable
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    // Horizontal tunnel. `min()` and `max()` are used in the case of `x1 > x2`
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    // Vertical tunnel
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
 }
