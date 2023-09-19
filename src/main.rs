c81d3fd45c917a211bef6e7b5d1bb3dfa9648cb5
kaubu <29447633+kaubu@users.noreply.github.com>
2023-09-19 08:56:39 +0000

    Update

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,23 @@ fn main() {
     let mut tcod = Tcod {
         root,
     };
+
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
+    while !tcod.root.window_closed() {
+        tcod.root.set_default_foreground(WHITE);
+        tcod.root.clear();
+        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root.flush();
+        tcod.root.wait_for_keypress(true);
+    }
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player: &mut i32) -> bool {
+    // todo: handle keys
+    
+    false
 }
